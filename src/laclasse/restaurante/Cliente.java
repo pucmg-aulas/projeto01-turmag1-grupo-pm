4aa5b0e1dc49f349447e96c30e69de7a85be839e
Giovanna Guasch <133532207+GioGuasch@users.noreply.github.com>
2024-06-09 23:30:42 -0300

    Update Cliente.java

--- src/laclasse/restaurante/Cliente.java
+++ src/laclasse/restaurante/Cliente.java
@@ -7,25 +7,23 @@ public class Cliente implements Serializable {
     private String nome;
     private String contato;
 
-    public Cliente(String rgCliente) {
-
+    public Cliente(String rgCliente, String nome, String contato) {
+        this.rg = rgCliente;
+        this.nome = nome;
+        this.contato = contato;
     }
-
 
     public Requisicao fazerRequisicao(int quantidadeClientes) {
         return new Requisicao(this, quantidadeClientes);
     }
 
-
     public String getRG() {
         return this.rg;
     }
 
-
     public String getNome() {
         return this.nome;
     }
-
 
     public String getContato() {
         return this.contato;
