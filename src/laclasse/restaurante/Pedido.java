fc700a708b6b9d100fcbb6257710def505bc4f20
Giovanna Guasch <133532207+GioGuasch@users.noreply.github.com>
2024-06-09 23:50:39 -0300

    Update Pedido.java

--- src/laclasse/restaurante/Pedido.java
+++ src/laclasse/restaurante/Pedido.java
@@ -1,62 +1,44 @@
-package laclasse.restaurante;
+public class Restaurante {
+    private List<Mesa> mesas;
+    private Cardapio cardapio;
 
-import java.util.ArrayList;
-import java.util.List;
+    public Restaurante() {
+        this.mesas = new ArrayList<>();
+        this.cardapio = new Cardapio();
 
-public class Pedido implements PedidoInterface {
-    private List<ItemMenu> pratos;
-    private List<ItemMenu> bebidas;
-
-
-    public Pedido() {
-        this.pratos = new ArrayList<>();
-        this.bebidas = new ArrayList<>();
-    }
-
-
-    @Override
-    public void adicionarPrato(ItemMenu prato) {
-        this.pratos.add(prato);
-    }
-
-
-    @Override
-    public void removerPrato(ItemMenu prato) {
-        this.pratos.remove(prato);
-    }
-
-
-    @Override
-    public void adicionarBebida(ItemMenu bebida) {
-        this.bebidas.add(bebida);
-    }
-
-    @Override
-    public void removerBebida(ItemMenu bebida) {
-        this.bebidas.remove(bebida);
-    }
-
-
-    @Override
-    public double calcularTotal() {
-        double total = 0;
-        for (ItemMenu prato : pratos) {
-            total += prato.getPreco();
+        // Adiciona 4 mesas com capacidade para 4 pessoas
+        for (int i = 0; i < 4; i++) {
+            this.mesas.add(new Mesa(i + 1, 4));
         }
-        for (ItemMenu bebida : bebidas) {
-            total += bebida.getPreco();
+
+        // Adiciona 4 mesas com capacidade para 6 pessoas
+        for (int i = 4; i < 8; i++) {
+            this.mesas.add(new Mesa(i + 1, 6));
         }
-        return total;
+
+        // Adiciona 2 mesas com capacidade para 8 pessoas
+        for (int i = 8; i < 10; i++) {
+            this.mesas.add(new Mesa(i + 1, 8));
+        }
+
+        // Adiciona pratos ao cardápio
+        this.cardapio.adicionarPrato(new ItemMenu("Moqueca de Tilápia", 20.00));
+        this.cardapio.adicionarPrato(new ItemMenu("Falafel Assado", 15.00));
+        this.cardapio.adicionarPrato(new ItemMenu("Salada Primavera com Macarrão Konjac", 12.00));
+        this.cardapio.adicionarPrato(new ItemMenu("Escondidinho de Frango", 18.00));
+        this.cardapio.adicionarPrato(new ItemMenu("Strogonoff", 22.00));
+        this.cardapio.adicionarPrato(new ItemMenu("Caçarola de carne com legumes", 25.00));
+
+        // Adiciona bebidas ao cardápio
+        this.cardapio.adicionarBebida(new ItemMenu("Água", 2.00));
+        this.cardapio.adicionarBebida(new ItemMenu("Suco", 5.00));
+        this.cardapio.adicionarBebida(new ItemMenu("Refrigerante", 4.00));
+        this.cardapio.adicionarBebida(new ItemMenu("Cerveja", 7.00));
+        this.cardapio.adicionarBebida(new ItemMenu("Taça de vinho", 10.00));
     }
 
-
-    @Override
-    public double calcularTotalComServico() {
-        return this.calcularTotal() * 1.10;
-    }
-
-
-    @Override
+    // Outros métodos...
+}
     public double calcularPorPessoa(int numeroDePessoas) {
         return this.calcularTotalComServico() / numeroDePessoas;
     }
